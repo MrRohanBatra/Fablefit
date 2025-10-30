66e5c3096de137245d9aec4f839c7f19a5fd427a
Rohan Batra <rohanbatra.in@gmail.com>
2025-10-30 09:35:28 +0530

    Update FirebaseAuth.js

--- src/components/FirebaseAuth.js
+++ src/components/FirebaseAuth.js
@@ -9,13 +9,22 @@ import {
 } from "firebase/auth";
 import { createContext, useEffect, useState } from "react"; // Unused 'Children' removed
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyAI7OwMffnqF7zIzMJOaUEZ6GM64raH31Y",
   authDomain: "ecomapp-fd4cc.firebaseapp.com",
   projectId: "ecomapp-fd4cc",
   storageBucket: "ecomapp-fd4cc.firebasestorage.app",
   messagingSenderId: "1072032319999",
   appId: "1:1072032319999:web:55fc9ed03c6d4942a6bfca",
+};
+*/
+const firebaseConfig = {
+  apiKey: "AIzaSyAc7GUsZbBO2FD0DzxT2U3EWdXXL5wfSqs",
+  authDomain: "fablefit-81e44.firebaseapp.com",
+  projectId: "fablefit-81e44",
+  storageBucket: "fablefit-81e44.firebasestorage.app",
+  messagingSenderId: "567522622987",
+  appId: "1:567522622987:web:32b6150206acd9e864510e"
 };
 
 // Export the context so other components can use it
