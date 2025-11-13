0eab314073255257c8ddf8e88996f0d2897b22e3
Rohan Batra <rohanbatra.in@gmail.com>
2025-11-13 11:58:32 +0530

    added local changes

--- test.js
+++ test.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 import Cart from "./models/cartModel.js";
 import dotenv from "dotenv";
+import Product from "./models/productModel.jsnpm ";
 dotenv.config();
 
 
 await mongoose.connect(process.env.MONGODB_URI);
-const data = await Cart.deleteMany({});
+const data = await Product.find({});
 console.log(data);
 await mongoose.disconnect();
