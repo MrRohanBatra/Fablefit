2800817bfc1dd47800c74dfb177a58ea00ac9b9b
Rohan Batra <rohanbatra.in@gmail.com>
2025-11-20 16:30:18 +0530

    fixed the structure

--- server/test.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import connectDB from "./db.js";
-import Cart from "./models/cartModel.js";
-
-await connectDB();
-await Cart.deleteMany({});
-console.log("Cart collection cleared.");
-mongoose.disconnect();
