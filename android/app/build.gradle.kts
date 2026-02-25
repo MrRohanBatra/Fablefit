76db981c67ae25d3836247ea3c7680f677c8b2d1
Rohan Batra <rohanbatra.in@gmail.com>
2026-02-25 11:20:23 +0530

    fixeds

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rohan.fablefit"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
